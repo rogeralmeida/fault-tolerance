80f47585035700e571201ecbec0fd4bceecc0de2
Roger Almeida <ralmeida@tyro.com>
2016-12-27 01:00:48 +1100

    Added hystrix command for foursquare

---
+++ src/main/java/com/github/rogeralmeida/faulttolerance/foursquare/command/SearchVenueCommand.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2003 - 2016 Tyro Payments Limited.
+ * Lv1, 155 Clarence St, Sydney NSW 2000.
+ * All rights reserved.
+ */
+package com.github.rogeralmeida.faulttolerance.foursquare.command;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import com.github.rogeralmeida.faulttolerance.foursquare.model.BodyResponse;
+import com.github.rogeralmeida.faulttolerance.foursquare.model.Venue;
+import com.github.rogeralmeida.faulttolerance.foursquare.model.VenueSearchResponse;
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+import lombok.extern.java.Log;
+
+@Log
+public class SearchVenueCommand extends HystrixCommand<Set<Venue>> {
+
+    private static final String URL = "https://api.foursquare.com/v2/venues/search?near=Sydney,AU" +
+                "&client_id=3TKSCYPHHW4MEHWS1QXC343USZXQN11GIJTHQRAHDL3XEUNX" +
+                "&v=20161223" +
+                "&client_secret=V5XQRGKG5PS41BZ5L3J4S00RM4RCQDDJ0UOIPCQ2BU5ZVYYN&query=%s";
+
+    private final String name;
+    private final RestTemplate restTemplate;
+
+    public SearchVenueCommand(String name, RestTemplate restTemplate) {
+        super(HystrixCommandGroupKey.Factory.asKey("FourSquare"));
+        this.name = name;
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    protected Set<Venue> run() throws Exception {
+        ResponseEntity<VenueSearchResponse> venueSearchResponseResponseEntity = null;
+        try {
+            venueSearchResponseResponseEntity = restTemplate.getForEntity(String.format(URL, URLEncoder.encode(name, "UTF-8")), VenueSearchResponse.class);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        VenueSearchResponse venueSearchResponse = venueSearchResponseResponseEntity.getBody();
+        BodyResponse response = venueSearchResponse.getResponse();
+        log.info("Response: " + response.toString());
+
+        return response.getVenues();
+    }
+
+    @Override
+    protected Set<Venue> getFallback() {
+        return new HashSet<>();
+    }
+}
--- src/main/java/com/github/rogeralmeida/faulttolerance/foursquare/services/FourSquareService.java
+++ src/main/java/com/github/rogeralmeida/faulttolerance/foursquare/services/FourSquareService.java
@@ -5,18 +5,14 @@
  */
 package com.github.rogeralmeida.faulttolerance.foursquare.services;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.github.rogeralmeida.faulttolerance.foursquare.model.BodyResponse;
+import com.github.rogeralmeida.faulttolerance.foursquare.command.SearchVenueCommand;
 import com.github.rogeralmeida.faulttolerance.foursquare.model.Venue;
-import com.github.rogeralmeida.faulttolerance.foursquare.model.VenueSearchResponse;
 
 import lombok.extern.java.Log;
 
@@ -24,28 +20,14 @@ import lombok.extern.java.Log;
 @Log
 public class FourSquareService {
 
-
-
-    public static final String URL = "https://api.foursquare.com/v2/venues/search?near=Sydney,AU" +
-            "&client_id=3TKSCYPHHW4MEHWS1QXC343USZXQN11GIJTHQRAHDL3XEUNX" +
-            "&v=20161223" +
-            "&client_secret=V5XQRGKG5PS41BZ5L3J4S00RM4RCQDDJ0UOIPCQ2BU5ZVYYN&query=%s";
     @Autowired
     private RestTemplate restTemplate;
 
     public Set<Venue> searchVenues(String name) {
 
-        ResponseEntity<VenueSearchResponse> venueSearchResponseResponseEntity = null;
-        try {
-            venueSearchResponseResponseEntity = restTemplate.getForEntity(String.format(URL, URLEncoder.encode(name, "UTF-8")), VenueSearchResponse.class);
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
-        VenueSearchResponse venueSearchResponse = venueSearchResponseResponseEntity.getBody();
-        BodyResponse response = venueSearchResponse.getResponse();
-        log.info("Response: " + response.toString());
+        SearchVenueCommand searchVenueCommand = new SearchVenueCommand(name, restTemplate);
 
-        return response.getVenues();
+        return searchVenueCommand.execute();
     }
 
 }
